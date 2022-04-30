48eea9a854f25cee985265cd503c2d226042fb0c
AndreYS82 <semenenko19.arh@gmail.com>
2022-04-30 22:58:35 +0300

    Add homework2

--- src/com/kum/Variables2.java
+++ src/com/kum/Variables2.java
@@ -2,4 +2,21 @@ package com.kum;
 
 public class Variables2 {
 
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+
+
+    }
 }
